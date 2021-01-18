11de02b33fb3e19225aa9e75d48fe71c78c7f49e
johndavid25 <75648891+johndavid25@users.noreply.github.com>
2021-01-18 03:05:31 -0500

    trying some functions

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,25 @@
 document.getElementById("solution").addEventListener("click", solution);
+document.getElementById("clear").addEventListener("click", clear);
+document.getElementById("default").addEventListener("click", defualt);
+
+function defualt () {
+    document.getElementById("loanAmount").value = "80000";
+    document.getElementById("term").value = "120";
+    document.getElementById("interestRate").value = "5";
+}
+
+
+function clear () {
+    document.getElementById("loanAmount").value = "";
+    document.getElementById("term").value = "";
+    document.getElementById("interestRate").value = "";
+}
+
 
 function solution () {
+    
+
+
     //Collecting user inputs
     let amountLoaned = parseInt(document.getElementById("loanAmount").value);
     let numMonths = parseInt(document.getElementById("term").value);
@@ -12,16 +31,20 @@ function solution () {
     document.getElementById("monthlyPayments").innerHTML = `$${monthlyPayments} `;
 
     //Total Principal
-    let totalPrinipal = amountLoaned.toFixed(2)
+    let totalPrinipal = amountLoaned.toFixed(2);
     document.getElementById("totalPrinipal").innerHTML = `$${totalPrinipal} `;
 
     //Total Interest
-    document.getElementById("totalInterest").innerHTML = `$${monthlyPayments} `;
+    let totalInterestAmount = (numMonths * monthlyPayments) - totalPrinipal;
+    let totalInterest = totalInterestAmount.toFixed(2);
+    document.getElementById("totalInterest").innerHTML = `$${totalInterest} `;
 
     //Total Cost
-    document.getElementById("totalCost").innerHTML = `$${monthlyPayments} `;
-    
-}
+    let totalCostAmount = amountLoaned + totalInterestAmount;
+    let totalCost = totalCostAmount.toFixed(2);
+    document.getElementById("totalCost").innerHTML = `$${totalCost} `;
 
+
+}
 
 
